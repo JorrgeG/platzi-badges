0df3a8e55558bec01dc45bb60493fd7be9c15c7f
Jorge Andres Gutierrez Chavez <cucugutierrez1@gmail.com>
2020-11-14 16:17:28 -0500

    Rutas

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg'
 import BadgesList from '../components/BadgesList';
@@ -44,7 +43,6 @@ class Badges extends React.Component{
     render() {
         return (
             <div>
-                <Navbar />
                 <div className="Badges">
                     <div className="Badges__hero">
                         <div className="Badges__container">
