f8a835942bee661121b517913738360c714c58a7
Jorge Andres Gutierrez Chavez <cucugutierrez1@gmail.com>
2020-11-14 20:01:27 -0500

    POST

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/Badge.css";
 
 import logoConf from "../images/badge-header.svg";
+import Gravatar from "../components/Gravatar";
 
 class Badge extends React.Component {
   render() {
@@ -14,7 +15,7 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src={avatarUrl} alt="Avatar" />
+          <Gravatar className="Badge__avatar" email={this.props.email}/>
           <h1>
             {firstName} <br />
             {lastName}
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import '../styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
+import './styles/BadgeNew.css';
+import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
+import api from '../pages/api';
 
 class BadgeNew extends React.Component {
   state = {
@@ -25,35 +26,52 @@ class BadgeNew extends React.Component {
     });
   };
 
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
   render() {
     return (
-      <div>
+      <React.Fragment>
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
+          <img
+            className="BadgeNew__hero-image img-fluid"
+            src={header}
+            alt="Logo"
+          />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                email={this.state.form.email}
-                avatarUrl="https://assets.stickpng.com/thumbs/5852cd5558215f0354495f66.png"
+                firstName={this.state.form.firstName || 'FIRST_NAME'}
+                lastName={this.state.form.lastName || 'LAST_NAME'}
+                twitter={this.state.form.twitter || 'twitter'}
+                jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+                email={this.state.form.email || 'EMAIL'}
+                avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
               />
             </div>
 
             <div className="col-6">
               <BadgeForm
                 onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
                 formValues={this.state.form}
               />
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
